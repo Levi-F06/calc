45c0f9c9ddb098adb3a0fe9ed22221cbc790d070
Levi-F06 <levi.flanagan06@gmail.com>
2024-11-23 14:38:54 +0000

    :sparkles: Add intro page

--- calc.js
+++ calc.js
@@ -1,1 +1,13 @@
-console.log("Hello world");
+
+const startBtn = document.querySelector("#startBtn");
+
+startBtn.addEventListener("click", () => {
+  startBtn.parentElement.classList.add("dissapear");  
+  setTimeout(showCalc, 1000);
+})
+
+function showCalc() {
+  startBtn.parentElement.innerHTML = "";
+  alert("Caclulator would be shown here");
+}
+
